8bbb09beaa0ad96ee4b97b3698edd8d6a9697074
luislopez <16031212@itcelaya.edu.mx>
2019-05-09 14:39:46 -0500

    .

--- src/sample/Login.java
+++ src/sample/Login.java
@@ -25,6 +25,9 @@ public class Login implements EventHandler {
     private Scene scene;
     private Menu menu;
 
+    public Login() {
+    }
+
     public Login(Stage stage) {
         this.nStage = stage;
         GridPane grid = new GridPane();
